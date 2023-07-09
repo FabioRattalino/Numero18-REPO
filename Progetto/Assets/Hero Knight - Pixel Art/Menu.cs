810c8cd347a4c32322537b737e22ff446e4a948a
FabioRattalino <fabio.rattalino@edu.itspiemonte.it>
2023-07-09 22:51:45 +0200

    aggiornamento menu

---
+++ Progetto/Assets/Hero Knight - Pixel Art/Menu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+     public void PlayGame(){
+      SceneManager.LoadScene(1);
+     }
+     
+     
+     public void Opzioni(){
+        
+     }
+
+    public void Esci(){
+        
+     }
+
+
+
+
+}
